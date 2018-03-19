2cc78ea5b88861e05f1a14277c517867b988f644
dingxuejin <761110187@qq.com>
2018-03-19 15:46:24 +0800

    1546

--- res/js/nav.js
+++ res/js/nav.js
@@ -3,11 +3,15 @@ $(".nav2").on("click", function () {
     let $li=$ul.children("li");
     let height = $ul.children("li").css("height");
     if (height === "0px") {
+        $(".nav1").css({border:'1px solid transparent'})
         $('.nav7').css( 'transform', 'rotateX(0deg)')
-        $('.nav1>li').animate({ height: 0 }, 300)
-        $li.animate({ height: 40 }, 300)
+        $('.nav1>li').animate({ height: 0}, 300)
+        $li.animate({ height: 40 ,}, 300)
         $(this).find('.nav7').css( 'transform', 'rotateX(180deg)')
+        $ul.css({border:'1px solid #B9DFFB'})
     } else {
+        $(".nav1").css({border:'1px solid transparent'})
+        $('.nav7').css( 'transform', 'rotateX(0deg)')
         $li.animate({ height: 0 }, 300,function(){
         })
     }
